5f1b75dd7e8e3df13be096e53f8daa1915303e28
ajandhayala3 <ajandhayala3@gatech.edu>
2022-10-31 02:20:02 -0400

    yo

---
+++ server/routers/events.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+let Event = require('../models/event.model');
+
+router.route('/').get((req, res) => {
+  Event.find()
+    .then(events => res.json(events))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const user = req.body.user;
+  const location = req.body.location;
+  const description = req.body.description;
+  const timeCreated = req.body.timeCreated;
+  const date = Date.parse(req.body.date);
+  const title = req.body.title;
+
+
+  const newEvent = new Event({
+    user,
+    location,
+    description,
+    timeCreated,
+    date,
+    title,
+  });
+
+  newEvent.save()
+  .then(() => res.json('Event added!'))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Event.findById(req.params.id)
+    .then(event => res.json(event))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Event.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Event deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Event.findById(req.params.id)
+    .then(event => {
+        event.user = req.body.user;
+        event.location = req.body.location;
+        event.description = req.body.description;
+        event.timeCreated = req.body.timeCreated;
+        event.date = Date.parse(req.body.date);
+        event.title = req.body.title;
+
+      event.save()
+        .then(() => res.json('Event updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
