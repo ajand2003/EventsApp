1d26bcd6105371f6f69cfc2b53ef88207c8ec05e
ajandhayala3 <ajandhayala3@gatech.edu>
2022-10-31 01:35:40 -0400

    new change

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,11 +17,9 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
 
-app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
